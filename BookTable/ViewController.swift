02af51e96316e21081b56629d8a7554b61b31cbd
Artem <moiseev@physics.msu.ru>
2023-07-30 22:01:25 +0300

    Initial Commit

---
+++ BookTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookTable
+//
+//  Created by Artem on 30.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
